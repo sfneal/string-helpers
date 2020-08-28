a11223f9c3cc9500ef9b05cb97d740aa195c9bae
Stephen Neal <stephen.neal14@gmail.com>
2020-08-28 11:46:05 -0400

    MAKE StringHelpers class

---
+++ src/StringHelpers.php
@@ -0,0 +1,221 @@
+<?php
+
+
+namespace Sfneal\Helpers\Strings;
+
+
+class StringHelpers
+{
+    /**
+     * @var string
+     */
+    private $string;
+
+    /**
+     * StringHelpers constructor.
+     *
+     * @param string $string
+     */
+    public function __construct(string $string)
+    {
+        $this->string = $string;
+    }
+
+    /**
+     * Sanitize a file name to remove illegal chars.
+     *
+     * @return string
+     */
+    public function sanitizeFileName(): string
+    {
+        return preg_replace('/[^A-Za-z0-9_\-]/', '_', $this->string);
+    }
+
+    /**
+     * Truncate a string to be no longer than $chars.
+     *
+     * @param int $chars
+     * @param string $suffix
+     * @return string
+     */
+    public function truncate(int $chars = 100, string $suffix = '...'): string
+    {
+        return (strlen($this->string) <= $chars) ? $this->string : substr($this->string, 0, $chars) . $suffix;
+    }
+
+    /**
+     * Covert a camel cased string into a $char separated string.
+     *
+     * @param string $char
+     * @return string
+     */
+    public function camelCaseConvert(string $char = '-'): string
+    {
+        return strtolower(preg_replace('/(?<!^)[A-Z]/', $char.'$0', $this->string));
+    }
+
+    /**
+     * Remove illegal characters from a string to create an ID.
+     *
+     * @return string
+     */
+    public function id(): string
+    {
+        return strtolower(
+            str_replace(' ', '-', str_replace('&', '-', $this->string))
+        );
+    }
+
+    /**
+     * Remove ' ', '-', '&' characters.
+     *
+     * @return string
+     */
+    public function strip()
+    {
+        return strtolower(
+            str_replace(
+                ',', '', str_replace(
+                    ' ', '-', str_replace(
+                        '&',
+                        '',
+                        $this->string
+                    )
+                )
+            )
+        );
+    }
+
+    /**
+     * Check if a needle string is in a haystack string.
+     *
+     * @param string $needle
+     * @return bool
+     */
+    public function inString(string $needle): bool
+    {
+        return strpos($this->string, $needle) !== false;
+    }
+
+    /**
+     * Determine if the $string is a plain text list of values.
+     *
+     *  - if $string is a list, an array of values is returned
+     *  - if $sting is a string, false is returned
+     *
+     * @param array|string[] $separators
+     * @return bool|false|string[]
+     */
+    public function isListString(array $separators = [', ', ' '])
+    {
+        foreach ($separators as $needle) {
+            // If the $needle is found in the $string
+            // we know the string is a plain text list
+            // so an array of values is returned
+            if ($this->inString($needle)) {
+                return explode($needle, $this->string);
+            }
+        }
+
+        // If none of the separators are found
+        // we know the string is not a list
+        return false;
+    }
+
+    /**
+     * Explode a string using an array of delimiters instead of a single string.
+     *
+     * @param array|string[] $delimiters
+     * @param string $replacer
+     * @return array
+     */
+    public function explodeMany(array $delimiters = [',', '&', '/', '-'], string $replacer = '***'): array
+    {
+        // Replace all of $delimiters chars with the
+        // $replacer so that a single delimiter can
+        // be used to explode the string
+        $cleaned = str_replace($delimiters, $replacer, $this->string);
+
+        // Use the replacer as the $delimiter
+        // since it has replaced each of the
+        // passed $delimiters
+        $split = explode($replacer, $cleaned);
+
+        // Utilize collection's if laravel/framework is installed
+        if (function_exists('collect')) {
+            // Collect the array of strings
+            return collect($split)
+
+                // Remove empty strings from the collection
+                ->filter(function (string $item) {
+                    return strlen($item) > 0;
+                })
+
+                // Trim the remaining strings in the collection
+                ->map(function (string $item) {
+                    return trim($item);
+                })
+
+                // Encode as array
+                ->toArray();
+        }
+
+        // Use built in array functions
+        else {
+            // Remove empty strings from the collection
+            $filtered = array_filter($split, function (string $item) {
+                return strlen($item) > 0;
+            });
+
+            // Trim the remaining strings in the collection
+            return  array_map(function (string $item) {
+                return trim($item);
+            }, $filtered);
+        }
+    }
+
+    /**
+     * Add whitespace padding to a string.
+     *
+     * @param int $amount
+     * @param string $pad
+     * @param bool $front add whitespace to 'front' of the string
+     * @param bool $back add whitespace to the 'back' of the string
+     * @return string
+     */
+    public function whitespacePad(int $amount = 1,
+                                  string $pad = ' ',
+                                  bool $front = true,
+                                  bool $back = true)
+    {
+        // Multiple $pad chars by $amount
+        $whitespace = str_repeat($pad, $amount);
+
+        // Return string with padding
+        return ($front ? $whitespace : '') . $this->string . ($back ? $whitespace : '');
+    }
+
+    /**
+     * Add whitespace padding to only the 'back' of the string.
+     *
+     * @param int $amount
+     * @param string $pad
+     * @return string
+     */
+    public function whitespacePadBack(int $amount = 1, string $pad = ' ')
+    {
+        return $this->whitespacePad($amount, $pad, false, true);
+    }
+
+    /**
+     * Add whitespace padding to only the 'back' of the string.
+     *
+     * @param int $amount
+     * @param string $pad
+     * @return string
+     */
+    function whitespacePadFront(int $amount = 1, string $pad = ' ')
+    {
+        return $this->whitespacePad($amount, $pad, true, false);
+    }
+}
--- src/strings.php
+++ src/strings.php
@@ -1,5 +1,7 @@
 <?php
 
+use Sfneal\Helpers\Strings\StringHelpers;
+
 /**
  * Sanitize a file name to remove illegal chars.
  *
@@ -8,8 +10,9 @@
  */
 function sanitizeFileName(string $raw): string
 {
-    return preg_replace('/[^A-Za-z0-9_\-]/', '_', $raw);
+    return (new StringHelpers($raw))->sanitizeFileName();
 }
+
 
 /**
  * Truncate a string to be no longer than $chars.
@@ -21,8 +24,9 @@ function sanitizeFileName(string $raw): string
  */
 function truncateString(string $string, int $chars = 100, string $suffix = '...'): string
 {
-    return (strlen($string) <= $chars) ? $string : substr($string, 0, $chars).$suffix;
+    return (new StringHelpers($string))->truncate($chars, $suffix);
 }
+
 
 /**
  * Covert a camel cased string into a $char separated string.
@@ -33,8 +37,130 @@ function truncateString(string $string, int $chars = 100, string $suffix = '...'
  */
 function camelCaseConverter(string $string, $char = '-'): string
 {
-    return strtolower(preg_replace('/(?<!^)[A-Z]/', $char.'$0', $string));
+    return (new StringHelpers($string))->camelCaseConvert($char);
 }
+
+
+/**
+ * Remove illegal characters from a string to create an ID.
+ *
+ * @param $string
+ * @return string
+ */
+function str2id($string)
+{
+    return (new StringHelpers($string))->id();
+}
+
+/**
+ * Remove ' ', '-', '&' characters.
+ *
+ * @param $item
+ * @return string
+ */
+function stripString($item)
+{
+    return (new StringHelpers($item))->strip();
+}
+
+/**
+ * Remove spaces and convert string to lowercase chars.
+ *
+ * @param $string
+ * @return string|string[]
+ */
+function stringID($string)
+{
+    return (new StringHelpers($string))->id();
+}
+
+/**
+ * Check if a needle string is in a haystack string.
+ *
+ * @param string $haystack
+ * @param string $needle
+ * @return bool
+ */
+function inString(string $haystack, string $needle): bool
+{
+    return (new StringHelpers($haystack))->inString($needle);
+}
+
+/**
+ * Determine if the $string is a plain text list of values.
+ *
+ *  - if $string is a list, an array of values is returned
+ *  - if $sting is a string, false is returned
+ *
+ * @param string $string
+ * @param array|string[] $separators
+ * @return bool|false|string[]
+ */
+function isListString(string $string, array $separators = [', ', ' '])
+{
+    return (new StringHelpers($string))->isListString($separators);
+}
+
+/**
+ * Explode a string using an array of delimiters instead of a single string.
+ *
+ * @param string $string
+ * @param array|string[] $delimiters
+ * @param string $replacer
+ * @return array
+ */
+function explodeMany(string $string,
+                     array $delimiters = [',', '&', '/', '-'],
+                     string $replacer = '***'): array
+{
+    return (new StringHelpers($string))->explodeMany($delimiters, $replacer);
+}
+
+/**
+ * Add whitespace padding to a string.
+ *
+ * @param string $string
+ * @param int $amount
+ * @param string $pad
+ * @param bool $front add whitespace to 'front' of the string
+ * @param bool $back add whitespace to the 'back' of the string
+ * @return string
+ */
+function whitespacePad(string $string,
+                       int $amount = 1,
+                       string $pad = ' ',
+                       bool $front = true,
+                       bool $back = true)
+{
+    return (new StringHelpers($string))->whitespacePad($amount, $pad, $front, $back);
+}
+
+/**
+ * Add whitespace padding to only the 'back' of the string.
+ *
+ * @param string $string
+ * @param int $amount
+ * @param string $pad
+ * @return string
+ */
+function whitespacePadBack(string $string, int $amount = 1, string $pad = ' ')
+{
+    return (new StringHelpers($string))->whitespacePadBack($amount, $pad);
+}
+
+/**
+ * Add whitespace padding to only the 'back' of the string.
+ *
+ * @param string $string
+ * @param int $amount
+ * @param string $pad
+ * @return string
+ */
+function whitespacePadFront(string $string, int $amount = 1, string $pad = ' ')
+{
+    return (new StringHelpers($string))->whitespacePadFront($amount, $pad);
+}
+
 
 /**
  * Implode values that are not null.
@@ -54,6 +180,7 @@ function implodeFiltered($glue, array $pieces)
     }
 }
 
+
 /**
  * Retrieve a website domain without prefixes.
  *
@@ -64,6 +191,7 @@ function extractWebsiteDomain(string $url)
 {
     return isset($url) ? str_replace('www.', '', parse_url($url)['host']) : '';
 }
+
 
 /**
  * Only retrieve an integer value if the $value is greater than zero.
@@ -83,131 +211,6 @@ function zero_replace($value, $substitute = '-', $return = null)
     return $value > 0 ? ($return ?? (int) $value) : $substitute;
 }
 
-/**
- * Remove illegal characters from a string to create an ID.
- *
- * @param $string
- * @return string
- */
-function str2id($string)
-{
-    return strtolower(str_replace(' ', '-', str_replace('&', '-', $string)));
-}
-
-/**
- * Remove ' ', '-', '&' characters.
- *
- * @param $item
- * @return string
- */
-function stripString($item)
-{
-    return strtolower(str_replace(',', '', str_replace(' ', '-', str_replace('&', '', $item))));
-}
-
-/**
- * Remove spaces and convert string to lowercase chars.
- *
- * @param $string
- * @return string|string[]
- */
-function stringID($string)
-{
-    return str_replace(' ', '-', strtolower($string));
-}
-
-/**
- * Check if a needle string is in a haystack string.
- *
- * @param string $haystack
- * @param string $needle
- * @return bool
- */
-function inString(string $haystack, string $needle): bool
-{
-    return strpos($haystack, $needle) !== false;
-}
-
-/**
- * Determine if the $string is a plain text list of values.
- *
- *  - if $string is a list, an array of values is returned
- *  - if $sting is a string, false is returned
- *
- * @param string $string
- * @param array|string[] $separators
- * @return bool|false|string[]
- */
-function isListString(string $string, array $separators = [', ', ' '])
-{
-    foreach ($separators as $needle) {
-        // If the $needle is found in the $string
-        // we know the string is a plain text list
-        // so an array of values is returned
-        if (inString($string, $needle)) {
-            return explode($needle, $string);
-        }
-    }
-
-    // If none of the separators are found
-    // we know the string is not a list
-    return false;
-}
-
-/**
- * Explode a string using an array of delimiters instead of a single string.
- *
- * @param string $string
- * @param array|string[] $delimiters
- * @param string $replacer
- * @return array
- */
-function explodeMany(string $string,
-                     array $delimiters = [',', '&', '/', '-'],
-                     string $replacer = '***'): array
-{
-    // Replace all of $delimiters chars with the
-    // $replacer so that a single delimiter can
-    // be used to explode the string
-    $cleaned = str_replace($delimiters, $replacer, $string);
-
-    // Use the replacer as the $delimiter
-    // since it has replaced each of the
-    // passed $delimiters
-    $split = explode($replacer, $cleaned);
-
-    // Utilize collection's if laravel/framework is installed
-    if (function_exists('collect')) {
-        // Collect the array of strings
-        return collect($split)
-
-            // Remove empty strings from the collection
-            ->filter(function (string $item) {
-                return strlen($item) > 0;
-            })
-
-            // Trim the remaining strings in the collection
-            ->map(function (string $item) {
-                return trim($item);
-            })
-
-            // Encode as array
-            ->toArray();
-    }
-
-    // Use built in array functions
-    else {
-        // Remove empty strings from the collection
-        $filtered = array_filter($split, function (string $item) {
-            return strlen($item) > 0;
-        });
-
-        // Trim the remaining strings in the collection
-        return  array_map(function (string $item) {
-            return trim($item);
-        }, $filtered);
-    }
-}
 
 /**
  * Pretty implode an array by using a different glue for the last piece.
@@ -240,55 +243,6 @@ function implodePretty(array $pieces, string $glue = ',', string $and = '&')
     // Implode comma separated $start with
     // & separated $last
     return implode(whitespacePad(trim($and)), [$start, $last]);
-}
-
-/**
- * Add whitespace padding to a string.
- *
- * @param string $string
- * @param int $amount
- * @param string $pad
- * @param bool $front add whitespace to 'front' of the string
- * @param bool $back add whitespace to the 'back' of the string
- * @return string
- */
-function whitespacePad(string $string,
-                       int $amount = 1,
-                       string $pad = ' ',
-                       bool $front = true,
-                       bool $back = true)
-{
-    // Multiple $pad chars by $amount
-    $whitespace = str_repeat($pad, $amount);
-
-    // Return string with padding
-    return ($front ? $whitespace : '').$string.($back ? $whitespace : '');
-}
-
-/**
- * Add whitespace padding to only the 'back' of the string.
- *
- * @param string $string
- * @param int $amount
- * @param string $pad
- * @return string
- */
-function whitespacePadBack(string $string, int $amount = 1, string $pad = ' ')
-{
-    return whitespacePad($string, $amount, $pad, false, true);
-}
-
-/**
- * Add whitespace padding to only the 'back' of the string.
- *
- * @param string $string
- * @param int $amount
- * @param string $pad
- * @return string
- */
-function whitespacePadFront(string $string, int $amount = 1, string $pad = ' ')
-{
-    return whitespacePad($string, $amount, $pad, true, false);
 }
 
 /**
