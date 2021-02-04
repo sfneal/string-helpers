d18dbbd7df12e01280671824acbc085f0193241d
Stephen Neal <stephen.neal14@gmail.com>
2021-02-04 15:22:49 -0500

    REFACTOR StaticHelpersTest to use StringHelpers import

--- tests/StaticHelpersTest.php
+++ tests/StaticHelpersTest.php
@@ -5,6 +5,7 @@ namespace Sfneal\Helpers\Strings\Tests;
 
 
 use PHPUnit\Framework\TestCase;
+use Sfneal\Helpers\Strings\StringHelpers;
 
 class StaticHelpersTest extends TestCase
 {
@@ -12,12 +13,12 @@ class StaticHelpersTest extends TestCase
     public function implodeFiltered()
     {
         $expected = 'Boston, MA';
-        $output = implodeFiltered(', ', ['Boston', 'MA']);
+        $output = StringHelpers::implodeFiltered(', ', ['Boston', 'MA']);
         $this->assertIsString($output);
         $this->assertEquals($expected, $output);
 
         $expected2 = 'Red-Blue-Green-Yellow';
-        $output2 = implodeFiltered('-', ['Red', 'Blue', null, 'Green', null, 'Yellow']);
+        $output2 = StringHelpers::implodeFiltered('-', ['Red', 'Blue', null, 'Green', null, 'Yellow']);
         $this->assertIsString($output2);
         $this->assertEquals($expected2, $output2);
     }
@@ -26,7 +27,7 @@ class StaticHelpersTest extends TestCase
     public function joinPaths()
     {
         $expected = 'home/user/docs/document.pdf';
-        $output = joinPaths('home', 'user', 'docs', 'document.pdf');
+        $output = StringHelpers::joinPaths('home', 'user', 'docs', 'document.pdf');
         $this->assertIsString($output);
         $this->assertEquals($expected, $output);
     }
@@ -36,7 +37,7 @@ class StaticHelpersTest extends TestCase
     {
         $url = 'https://superuser.com/questions/385653/batch-producing-pdfs-from-a-sequence-of-urls-multiple-urls';
         $expected = 'superuser.com';
-        $output = extractWebsiteDomain($url);
+        $output = StringHelpers::extractWebsiteDomain($url);
         $this->assertIsString($output);
         $this->assertEquals($expected, $output);
     }
@@ -44,28 +45,28 @@ class StaticHelpersTest extends TestCase
     /** @test */
     public function zero_replace()
     {
-        $this->assertEquals('-', zero_replace(0));
-        $this->assertEquals(1021, zero_replace(1021));
-        $this->assertEquals('*', zero_replace(0, '*'));
-        $this->assertEquals(546, zero_replace(546, '*'));
-        $this->assertEquals('blank', zero_replace(546, '-', 'blank'));
+        $this->assertEquals('-', StringHelpers::zero_replace(0));
+        $this->assertEquals(1021, StringHelpers::zero_replace(1021));
+        $this->assertEquals('*', StringHelpers::zero_replace(0, '*'));
+        $this->assertEquals(546, StringHelpers::zero_replace(546, '*'));
+        $this->assertEquals('blank', StringHelpers::zero_replace(546, '-', 'blank'));
     }
 
     /** @test */
     public function implodePretty()
     {
         $expected = 'First, Second & Third';
-        $output = implodePretty(['First', 'Second', 'Third']);
+        $output = StringHelpers::implodePretty(['First', 'Second', 'Third']);
         $this->assertIsString($output);
         $this->assertEquals($expected, $output);
 
         $expected = 'First & Second';
-        $output = implodePretty(['First', 'Second']);
+        $output = StringHelpers::implodePretty(['First', 'Second']);
         $this->assertIsString($output);
         $this->assertEquals($expected, $output);
 
         $expected = 'First; Second + Third';
-        $output = implodePretty(['First', 'Second', 'Third'], ';', '+');
+        $output = StringHelpers::implodePretty(['First', 'Second', 'Third'], ';', '+');
         $this->assertIsString($output);
         $this->assertEquals($expected, $output);
     }
